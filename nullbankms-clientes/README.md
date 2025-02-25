748453e9a19f36888db27f1f3758fcc49accef8f
igcozmu <129238122+igcozmu@users.noreply.github.com>
2025-02-25 14:18:46 -0300

    Delete nullbankms-clientes directory

--- nullbankms-clientes/README.md
+++
@@ -1,3 +0,0 @@
-# Componente de Clientes - Nullbank Manager System
-
-Modulo de controle parcial dos clientes do banco Nullbank.
--- nullbankms-clientes/backend/config.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import ini from 'ini';
-
-// Lendo o arquivo config.ini
-const config = ini.parse(fs.readFileSync('../config.ini', 'utf-8'));
-
-export default config;
